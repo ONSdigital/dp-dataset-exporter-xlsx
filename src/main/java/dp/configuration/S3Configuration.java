1109ab4e2f9ab85a8c83d35b6262b2a9ada5bf02
Matt Guest <mattguest1988@gmail.com>
2017-10-12 09:34:34 +0100

    Setup S3 credential to use IAM rules

--- src/main/java/dp/configuration/S3Configuration.java
+++ src/main/java/dp/configuration/S3Configuration.java
@@ -13,26 +13,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class S3Configuration {
 
-    @Value("${AWS_ACCESS_KEY_ID:access-key-id}")
-    private String accessKey;
-
-    @Value("${AWS_SECRET_ACCESS_KEY:access-key-secret}")
-    private String secretKey;
-
     @Value("${S3_REGION:eu-west-1}")
     private String region;
 
     @Bean
-    public BasicAWSCredentials basicAWSCredentials() {
-        return new BasicAWSCredentials(accessKey, secretKey);
-    }
-
-    @Bean
-    public AmazonS3 amazonS3Client(AWSCredentials awsCredentials) {
-        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
+    public AmazonS3 amazonS3Client() {
         AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
                 .withRegion(Regions.fromName(region))
-                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
                 .build();
         return s3Client;
     }
