bfe848d33cf8f067880fc9264718d66a9cc86d6f
Carl Hembrough <carl@bitwaves.co.uk>
2017-11-28 13:35:19 +0000

    Add title field to metadata class.

--- src/main/java/dp/api/dataset/Metadata.java
+++ src/main/java/dp/api/dataset/Metadata.java
@@ -1,7 +1,21 @@
 package dp.api.dataset;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * Metadata returned from the dataset API related to a specific version.
  */
 public class Metadata {
+
+
+    @JsonProperty("title")
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
