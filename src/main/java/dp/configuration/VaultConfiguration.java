be964d28401c27b89d0f07e39f12ebb6f19026b2
Dave Llewellyn <439837@gmail.com>
2019-10-08 09:56:57 +0100

    fixed empty string check

--- src/main/java/dp/configuration/VaultConfiguration.java
+++ src/main/java/dp/configuration/VaultConfiguration.java
@@ -1,42 +1,43 @@
 package dp.configuration;
 
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
-
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.vault.authentication.ClientAuthentication;
 import org.springframework.vault.authentication.TokenAuthentication;
 import org.springframework.vault.client.VaultEndpoint;
 import org.springframework.vault.config.AbstractVaultConfiguration;
 
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 @Configuration
 public class VaultConfiguration extends AbstractVaultConfiguration {
 
-	@Override
-	public VaultEndpoint vaultEndpoint() {
-		VaultEndpoint vaultEndpoint = null;
-		String addr = System.getenv("VAULT_ADDR");
-		if (addr.length() == 0) {
-			addr = "http://localhost:8200";
-		}
-		
-		try {
-			vaultEndpoint = VaultEndpoint.from(new URL(addr).toURI());
-		} catch (MalformedURLException | URISyntaxException e) {
-			throw new RuntimeException(e);
-		}
-		
-		return vaultEndpoint;
-	}
+    @Override
+    public VaultEndpoint vaultEndpoint() {
+        VaultEndpoint vaultEndpoint = null;
+        String addr = System.getenv("VAULT_ADDR");
+        if (StringUtils.isEmpty(addr)) {
+            addr = "http://localhost:8200";
+        }
 
-	@Override
-	public ClientAuthentication clientAuthentication() {
-		String token = System.getenv("VAULT_TOKEN");
-		if (token.length() == 0) {
-			token = "...";
-		}
-		return new TokenAuthentication(token);
-	}
+        try {
+            vaultEndpoint = VaultEndpoint.from(new URL(addr).toURI());
+        } catch (MalformedURLException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        return vaultEndpoint;
+    }
+
+    @Override
+    public ClientAuthentication clientAuthentication() {
+        String token = System.getenv("VAULT_TOKEN");
+        if (StringUtils.isEmpty(token)) {
+            token = "...";
+        }
+        return new TokenAuthentication(token);
+    }
 
 }
