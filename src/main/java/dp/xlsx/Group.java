e1da8feb1d9634d73df87046077bd4f146d7f78a
Dave Llewellyn <439837@gmail.com>
2021-06-11 08:14:54 +0100

    fixed incorrect equals method and updated dependency audit failure with exclude

--- src/main/java/dp/xlsx/Group.java
+++ src/main/java/dp/xlsx/Group.java
@@ -2,13 +2,12 @@ package dp.xlsx;
 
 import dp.api.dataset.models.CodeList;
 import dp.api.dataset.models.Metadata;
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.builder.EqualsBuilder;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class Group implements Comparable<Group> {
 
@@ -81,35 +80,23 @@ public class Group implements Comparable<Group> {
 
     @Override
     public int hashCode() {
-        /**
-         * Generate a string representation of the group by concatinating the string values of each
-         * {@link DimensionData}. The hashcode of this string is the hashcode for this object.
-         *
-         * Explanation:
-         * The previous hashcode implementation for {@link Group} used the hash code of the
-         * {@link Group}{@link #getGroupValues()} but this hashcode was not specific enough when two
-         * {@link DimensionData} objects had very similar values.
-         *
-         * The Group objects created from the following CSV rows although different were generating the same hashcode
-         * value:
-         *
-         *      ,.,1978-to-2018-19,1978 to 2018-19,K02000001,United Kingdom,18,18,gross-income,Gross income,1990s,1990s
-         *      ,.,1978-to-2018-19,1978 to 2018-19,K02000001,United Kingdom,19,19,gross-income,Gross income,1980s,1980s
-         *
-         * This hashcode collision resulted in the {@link V4File} incorrecting parsing the V4 file leading to missing
-         * rows or rows with incorrect observations being assigned.
-         * This slightly wierd approach attempts to address that problem. See GroupTest for me details.
-         */
-        return StringUtils.join(getGroupValues()
-                .stream()
-                .map(val -> val.toString())
-                .collect(Collectors.toList()), "|")
-                .hashCode();
+        return getGroupValues().hashCode();
     }
 
+
+    /**
+     * 2 groups are considered to be equal if their {@link Group#getGroupValues()} are equal. The obeservation values
+     * are not considered as part of the equality check.
+     */
     @Override
-    public boolean equals(Object object) {
-        return this.hashCode() == object.hashCode();
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+
+        if (o == null || this.getClass() != o.getClass()) return false;
+
+        final Group group = (Group) o;
+
+        return new EqualsBuilder().append(this.getGroupValues(), group.getGroupValues()).isEquals();
     }
 
     protected List<DimensionData> getGroupValues() {
--- src/test/java/dp/xlsx/GroupTest.java
+++ src/test/java/dp/xlsx/GroupTest.java
@@ -1,55 +1,73 @@
 package dp.xlsx;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GroupTest {
 
-    @Test
-    public void test_hashCode_verySimilarDimensions_shouldHaveDifferentCodes() throws Exception {
-        List<DimensionData> rowOne = new ArrayList<DimensionData>() {{
+    private Group indirectTaxesAge18The90s;
+    private Group indirectTaxesAge19The80s;
+    private Group clothing;
+    private Group alcoholicBeverages;
+
+    @Before
+    public void setup() {
+        indirectTaxesAge18The90s = new Group(new ArrayList<DimensionData>() {{
             add(new DimensionData(DimensionType.GEOGRAPHY, "United Kingdom", "K02000001"));
             add(new DimensionData(DimensionType.OTHER, "18", "18"));
             add(new DimensionData(DimensionType.OTHER, "Indirect taxes", "indirect-taxes"));
             add(new DimensionData(DimensionType.OTHER, "1990s", "1990s"));
-        }};
+        }}, null);
 
-        List<DimensionData> rowTwo = new ArrayList<DimensionData>() {{
+        indirectTaxesAge19The80s = new Group(new ArrayList<DimensionData>() {{
             add(new DimensionData(DimensionType.GEOGRAPHY, "United Kingdom", "K02000001"));
             add(new DimensionData(DimensionType.OTHER, "19", "19"));
             add(new DimensionData(DimensionType.OTHER, "Indirect taxes", "indirect-taxes"));
             add(new DimensionData(DimensionType.OTHER, "1980s", "1980s"));
-        }};
+        }}, null);
 
-        Group groupOne = new Group(rowOne, null);
-        Group groupTwo = new Group(rowTwo, null);
+        clothing = new Group(new ArrayList<DimensionData>() {{
+            add(new DimensionData(DimensionType.GEOGRAPHY, "K02000001", "K02000001"));
+            add(new DimensionData(DimensionType.OTHER, "cpi1dim1G30100", "03.1 Clothing"));
+        }}, null);
 
-        assertThat(groupOne.hashCode()).isNotEqualTo(groupTwo.hashCode());
+        alcoholicBeverages = new Group(new ArrayList<DimensionData>() {{
+            add(new DimensionData(DimensionType.GEOGRAPHY, "K02000001", "K02000001"));
+            add(new DimensionData(DimensionType.OTHER, "cpi1dim1G20100", "02.1 Alcoholic beverages"));
+        }}, null);
     }
 
     @Test
-    public void test_hashCode_sameDimensions_shouldHaveTheSameCode() throws Exception {
-        List<DimensionData> rowOne = new ArrayList<DimensionData>() {{
-            add(new DimensionData(DimensionType.GEOGRAPHY, "United Kingdom", "K02000001"));
-            add(new DimensionData(DimensionType.OTHER, "18", "18"));
-            add(new DimensionData(DimensionType.OTHER, "Indirect taxes", "indirect-taxes"));
-            add(new DimensionData(DimensionType.OTHER, "1990s", "1990s"));
-        }};
+    public void testEquals_verySimilarDimensions_shouldNotBeEqual() throws Exception {
+        assertThat(indirectTaxesAge18The90s).isNotEqualTo(indirectTaxesAge19The80s);
+    }
 
-        List<DimensionData> rowTwo = new ArrayList<DimensionData>() {{
-            add(new DimensionData(DimensionType.GEOGRAPHY, "United Kingdom", "K02000001"));
-            add(new DimensionData(DimensionType.OTHER, "18", "18"));
-            add(new DimensionData(DimensionType.OTHER, "Indirect taxes", "indirect-taxes"));
-            add(new DimensionData(DimensionType.OTHER, "1990s", "1990s"));
-        }};
+    @Test
+    public void testEquals_twoObjectWithSameValues_shouldBeEqual() {
+        Group dupplicate = new Group(new ArrayList<DimensionData>() {{
+            add(new DimensionData(DimensionType.GEOGRAPHY, "K02000001", "K02000001"));
+            add(new DimensionData(DimensionType.OTHER, "cpi1dim1G20100", "02.1 Alcoholic beverages"));
+        }}, null);
 
-        Group groupOne = new Group(rowOne, null);
-        Group groupTwo = new Group(rowTwo, null);
+        assertThat(alcoholicBeverages).isEqualTo(dupplicate);
+    }
 
-        assertThat(groupOne.hashCode()).isEqualTo(groupTwo.hashCode());
+    @Test
+    public void testEquals_twoObjectWithDifferentValues_shouldNotBeEqual() {
+        assertThat(clothing).isNotEqualTo(alcoholicBeverages);
+    }
+
+    @Test
+    public void testHashCode_twoEqualObjects_shouldProduceEqualHashcode() {
+        Group dupplicate = new Group(new ArrayList<DimensionData>() {{
+            add(new DimensionData(DimensionType.GEOGRAPHY, "K02000001", "K02000001"));
+            add(new DimensionData(DimensionType.OTHER, "cpi1dim1G20100", "02.1 Alcoholic beverages"));
+        }}, null);
+
+        assertThat(alcoholicBeverages).isEqualTo(dupplicate);
     }
 }
